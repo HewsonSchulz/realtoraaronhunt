11989a897f760445013d2afd94e69791ced200c2
Hewson Schulz <hewsonschulz@gmail.com>
2024-07-15 12:13:33 -0500

    date implementation and error handling

--- server/realtoraaronhuntapi/views/submit.py
+++ server/realtoraaronhuntapi/views/submit.py
@@ -6,12 +6,12 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import send_mail
 from django.conf import settings
 from rest_framework import status
-from .view_utils import calc_missing_props
+from .view_utils import calc_missing_props, get_date_info
 
 
 @csrf_exempt
 def submit(request):
-    '''info submition
+    '''info submission
 
     expected input:
     { 'name': 'Hewson Schulz',
@@ -21,67 +21,71 @@ def submit(request):
     '''
 
     if request.method == 'POST':
-
         try:
             req_body = json.loads(request.body)
         except JSONDecodeError as ex:
             return JsonResponse(
-                {
-                    'message': 'Your request contains invalid json',
-                },
+                {'message': 'Your request contains invalid json', 'error': ex},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        missing_props_msg = calc_missing_props(req_body, ['name', 'email', 'phone_num'])
-        if missing_props_msg:
-            return JsonResponse(
-                {'valid': False, 'message': missing_props_msg},
-                status=status.HTTP_400_BAD_REQUEST,
+        try:
+            missing_props_msg = calc_missing_props(
+                req_body, ['name', 'email', 'phone_num']
             )
+            if missing_props_msg:
+                return JsonResponse(
+                    {'valid': False, 'message': missing_props_msg},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
-        name = req_body['name'].strip()
-        email = req_body['email'].strip()
-        phone_num = req_body['phone_num'].strip()
-        agent_name = req_body.get('agent_name', None)
+            name = req_body['name'].strip()
+            email = req_body['email'].strip()
+            phone_num = req_body['phone_num'].strip()
+            agent_name = req_body.get('agent_name', None)
+            date = f'''{get_date_info()['date']} {get_date_info()['mer_time']}'''
 
-        # prepare email subject and message
-        subject = f'''New contact info from {name}'''
-        message = f'''Name: {name}
+            # prepare email subject and message
+            subject = f'''New contact info from {name}'''
+            message = f'''Name: {name}
 Email: {email}
 Phone Number: {phone_num}'''
 
-        if agent_name is not None:
-            message += f'''
+            if agent_name is not None:
+                message += f'''
 Currently working with an agent.
-Agent's name: {agent_name.strip()}'''
-        else:
-            message += '''
-Not currently working with an agent.'''
+Agent's name: {agent_name.strip()}
+Date: {date}'''
+            else:
+                message += f'''
+Not currently working with an agent.
+Date: {date}'''
 
-        # send email
-        send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=os.getenv('RECIPIENT_EMAILS', '').split(','),
-            fail_silently=False,
-        )
-
-        if agent_name is not None:
-            return JsonResponse(
-                {
-                    'valid': True,
-                    'name': name,
-                    'email': email,
-                    'phone_num': phone_num,
-                    'agent_name': agent_name.strip(),
-                },
-                status=status.HTTP_201_CREATED,
+            # send email
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=os.getenv('RECIPIENT_EMAILS', '').split(','),
+                fail_silently=False,
             )
-        else:
+
+            response_data = {
+                'valid': True,
+                'name': name,
+                'email': email,
+                'phone_num': phone_num,
+                'date': date,
+            }
+
+            if agent_name is not None:
+                response_data['agent_name'] = agent_name.strip()
+
+            return JsonResponse(response_data, status=status.HTTP_201_CREATED)
+        except Exception as ex:
             return JsonResponse(
-                {'valid': True, 'name': name, 'email': email, 'phone_num': phone_num},
-                status=status.HTTP_201_CREATED,
+                {'error': ex.args[0]},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
     else:
         return HttpResponseNotAllowed(['POST'])
--- server/realtoraaronhuntapi/views/view_utils.py
+++ server/realtoraaronhuntapi/views/view_utils.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+
+
 def calc_missing_props(req_body, missing_props):
     missing = [
         prop
@@ -7,3 +10,44 @@ def calc_missing_props(req_body, missing_props):
     if missing:
         return f'''Missing propert{'y' if len(missing) == 1 else 'ies'}: {', '.join(missing)}'''
     return None
+
+
+# generates an object with info about given date
+def get_date_info(datetime_str=None):
+    if datetime_str:
+        try:
+            # try parsing with date and time
+            datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            # try parsing with only date
+            datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%d')
+    else:
+        # TODO: make this not have to manually adjust by 1 hour
+        datetime_obj = datetime.now() - timedelta(hours=1)
+
+    year = datetime_obj.year
+    month = datetime_obj.strftime('%B')
+    month_num = datetime_obj.month
+    day = datetime_obj.day
+    weekday = datetime_obj.strftime('%A')
+    datetime_str = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
+    time = datetime_obj.strftime('%H:%M')
+    hour = datetime_obj.strftime('%I')
+    minute = datetime_obj.strftime('%M')
+    am_pm = datetime_obj.strftime('%p').lower()
+    mer_time = f'''{hour.lstrip('0')}:{minute}{am_pm}'''
+    date_str = datetime_obj.strftime('%Y-%m-%d')
+
+    date_info = {
+        'year': year,
+        'month': month,
+        'month_num': month_num,
+        'day': day,
+        'weekday': weekday,
+        'datetime': datetime_str,
+        'date': date_str,
+        'time': time,
+        'mer_time': mer_time,
+    }
+
+    return date_info
